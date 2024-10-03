290e13cd3bc63c34b04907e8cee67887f0c779f6
Gabriel26-univille <132339190+Gabriel26-univille@users.noreply.github.com>
2024-10-03 00:15:49 +0000

    first

---
+++ microservavaliacaoinstitucional/src/test/java/br/univille/microservavaliacaoinstitucional/MicroservavaliacaoinstitucionalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.microservavaliacaoinstitucional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservavaliacaoinstitucionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
